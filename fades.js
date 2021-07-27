3224be115a845361b7e3ce2de0bcd2b04f24b209
jmathis7113 <jmathis7113@gmail.com>
2021-07-26 23:53:04 -0400

    added js fades

--- fades.js
+++ fades.js
@@ -1,20 +1,38 @@
 const hirebtn = document.querySelector(".floatingHire");
-const hiresection = document.querySelector(".upworkWrapper")
+const hiresection = document.querySelector(".upworkWrapper");
+const sectionFade = document.querySelectorAll(".sectionFade");
+const sectionObserved = document.querySelectorAll(".sectionObserved");
 
 const options = {
     threshold: 0.40
 };
 
-const observer = new IntersectionObserver(function(entries, observer) {
+const sectionoptions = {
+    threshold: .40
+};
+
+const hirebtnobserver = new IntersectionObserver(function(entries, hirebtnobserver) {
     entries.forEach(entry => {
         if (!entry.isIntersecting) {
             hirebtn.classList.remove("disapear");
-            console.log();
         } else {
             hirebtn.classList.add("disapear")
-            console.log();
         }
     });
 }, options);
 
-observer.observe(hiresection);
+const sectionObserver = new IntersectionObserver(function (entries, sectionObserver) {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            entry.target.classList.add("fadeout");
+        } else {
+            entry.target.classList.remove("fadeout");
+            entry.target.classList.add("fadein");
+        }
+    });
+}, sectionoptions);
+
+hirebtnobserver.observe(hiresection);
+sectionFade.forEach(sectionFade => {
+    sectionObserver.observe(sectionFade);
+});
